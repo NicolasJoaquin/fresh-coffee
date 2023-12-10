529c0d984db735894c8162eab24a1d7f82805087
Nicolas Diorio <nicolas.joaquin.diorio@gmail.com>
2023-12-10 00:08:12 -0300

    product development (model, controller, resource, route and migration)

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
 /*
@@ -19,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/products', ProductController::class);
